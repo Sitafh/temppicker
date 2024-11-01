64316dbcecda975c1fb256ffb0bfffb27cb87e20
Sitafh <sunintaurusandfifthhouse@gmail.com>
2024-11-01 06:55:40 -0400

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -1,31 +1,9 @@
 @typedef {{name: string, key: string, tooltip?: string, checked?: boolean, sub?: {name: string, tooltip?: string, checked?: string}[]}[]} Options
 @typedef {{name: string, img: string, opts: Object<string, boolean|number[]}[]} CharData
  
- @type {Object.<string, {options: Options, characterData: CharData}>}
+@type {Object.<string, {options: Options, characterData: CharData}>}
  
-const dataSet = {
-	{
-		name: "Youji Sakiyama"
-		img: "ZwrpVCLeoyOpZGhy/youji.png",
-		opts: {
-			schoolboy: true,
-			}
-	}
-	{
-		name: "Tetsuo Shironuma",
-		img: "ZwrpVCLeoyOpZGhy/tetsuo.png",
-		opts: {
-			schoolboy: true,
-		}
-	}
-	{
-		name: 'Schoolboy',
-		key: 'schoolboy,
-		tooltip?: 'This removes the schoolboys.', // optional
-		checked?: false, // optional
-		}
-    }
-};
+const dataSet = {};
 
 /** 
  * Data set version, in YYYY-MM-DD form.
