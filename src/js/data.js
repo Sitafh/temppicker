9519466dabbb4bbc7c7851f1b7b87b93fa8f2b0e
Sitafh <sunintaurusandfifthhouse@gmail.com>
2024-11-01 06:40:40 -0400

    Add files via upload

---
+++ src/js/data.js
@@ -0,0 +1,45 @@
+/**
+ * @typedef {{name: string, key: string, tooltip?: string, checked?: boolean, sub?: {name: string, tooltip?: string, checked?: string}[]}[]} Options
+ * @typedef {{name: string, img: string, opts: Object<string, boolean|number[]}[]} CharData
+*/
+
+/**
+ * Data set. Characters will be removed from the sorting array based on selected options, working down the array.
+ * 
+ * @type {Object.<string, {options: Options, characterData: CharData}>}
+*/
+const dataSet = {
+	{
+		name: "Youji Sakiyama"
+		img: "ZwrpVCLeoyOpZGhy/youji.png",
+		opts: {
+			schoolboy: true,
+			}
+	}
+	{
+		name: "Tetsuo Shironuma",
+		img: "ZwrpVCLeoyOpZGhy/tetsuo.png",
+		opts: {
+			schoolboy: true,
+		}
+	}
+	{
+		name: 'Schoolboy',
+		key: 'schoolboy,
+		tooltip?: 'This removes the schoolboys.', // optional
+		checked?: false, // optional
+		}
+    }
+};
+
+/** 
+ * Data set version, in YYYY-MM-DD form.
+ * 
+ * @example '2018-02-20'
+*/
+let dataSetVersion = '2024-11-01';
+
+/**
+ * Image root, will be appended to the start of every image URL.
+ */
+const imageRoot = 'https://file.garden/';
